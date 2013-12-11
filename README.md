57a8276b1593196b5fce37c57f19ada894ca5fa3
Richard Hart <richard@ur-ban.com>
2013-12-11 12:57:35 +0000

    Tidy up

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 SpreeCurrentStockReport
 =======================
 
-Introduction goes here.
+Current stock report.
 
 Installation
 ------------
@@ -9,7 +9,8 @@ Installation
 Add spree_current_stock_report to your Gemfile:
 
 ```ruby
-gem 'spree_current_stock_report'
+gem 'spree_current_stock_report', github: "Hates/spree_free_item_action"
+
 ```
 
 Bundle your dependencies and run the installation generator:
@@ -18,23 +19,3 @@ Bundle your dependencies and run the installation generator:
 bundle
 bundle exec rails g spree_current_stock_report:install
 ```
-
-Testing
--------
-
-Be sure to bundle your dependencies and then create a dummy test app for the specs to run against.
-
-```shell
-bundle
-bundle exec rake test_app
-bundle exec rspec spec
-```
-
-When testing your applications integration with this extension you may use it's factories.
-Simply add this require statement to your spec_helper:
-
-```ruby
-require 'spree_current_stock_report/factories'
-```
-
-Copyright (c) 2013 [name of extension creator], released under the New BSD License
--- lib/spree_current_stock_report.rb
+++ lib/spree_current_stock_report.rb
@@ -1,2 +1,111 @@
 require 'spree_core'
 require 'spree_current_stock_report/engine'
+
+class SpreeCurrentStockReport
+
+  START_DATE = Date.parse("1/8/2013")
+
+  def self.report
+    new.report
+  end
+
+  def report
+    report_columns = [
+      "Variant ID",
+      "SKU",
+      "Barcode",
+      "Enabled",
+      "Brand",
+      "Name",
+      "Variant",
+      "Size",
+      "Current Stock",
+      "Sales (Total)",
+      "Sales (Past 30 Days)",
+      "Sales (Per Month Avg.)",
+      "Opt. Order Qty",
+      "Cost",
+      "Total Cost",
+      "Price",
+      "Total Price"
+    ]
+
+    totals = {}
+    line_items = orders(created_at_gt: START_DATE.to_s(:sql)).collect(&:line_items).flatten
+    line_items.each do |line_item|
+      next if line_item.variant.deleted?
+
+      variant = line_item.variant
+      totals[variant.id] = { variant: variant, quantity_sold: 0 } unless totals[variant.id]
+      totals[variant.id][:quantity_sold] += line_item.quantity
+    end
+
+    thirty_day_totals = {}
+    line_items = orders(created_at_gt: 30.days.ago.to_s(:sql)).collect(&:line_items).flatten
+    line_items.each do |line_item|
+      next if line_item.variant.deleted?
+
+      variant = line_item.variant
+      thirty_day_totals[variant.id] = { variant: variant, quantity_sold: 0 } unless thirty_day_totals[variant.id]
+      thirty_day_totals[variant.id][:quantity_sold] += 1
+    end
+
+    report = Table report_columns
+    variants.each do |v|
+      on_hand = v.total_on_hand || 0
+      cost = v.cost_price || BigDecimal.new("0.0")
+      price = v.price || BigDecimal.new("0.0")
+
+      total = totals[v.id]
+      sold = total ? total[:quantity_sold] : 0
+
+      thirty_day_total = thirty_day_totals[v.id]
+      thirty_day_sold = thirty_day_total ? thirty_day_total[:quantity_sold] : 0
+
+      report_attributes = []
+      report_attributes << v.id
+      report_attributes << "'#{v.sku}'"
+      report_attributes << "'#{v.barcode}'"
+      report_attributes << v.product.enabled
+      report_attributes << v.product.brand_taxon_root.try(:name)
+      report_attributes << v.product.name
+      report_attributes << v.options_text
+      report_attributes << v.size
+      report_attributes << on_hand
+
+      if total
+        report_attributes <<(sold)
+      else
+        report_attributes << "0"
+      end
+
+      if thirty_day_total
+        report_attributes <<(thirty_day_sold)
+      else
+        report_attributes << "0"
+      end
+
+      if total
+        current_date = Time.zone.now
+        number_of_months = (current_date.year * 12 + current_date.month) - (START_DATE.year * 12 + START_DATE.month)
+        sold_per_month = sold.to_f / number_of_months 
+
+        report_attributes <<(sold_per_month).round(2)
+        report_attributes <<(sold_per_month - on_hand).round(2)
+      else
+        report_attributes << "0"
+        report_attributes << "0"
+      end
+
+      report_attributes << cost.to_s
+      report_attributes << (cost * BigDecimal.new(on_hand.to_s)).to_s
+      report_attributes << price.to_s
+      report_attributes << (price * BigDecimal.new(on_hand.to_s)).to_s
+
+      report << report_attributes
+    end
+
+    report
+  end
+
+end
